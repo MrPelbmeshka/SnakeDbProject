ecc8aa9e4bb4cf48efedfe0fcf9af313788f8b36
viagra <Artemarkapad@yandex.ru>
2024-01-08 17:54:39 +0400

    Добавил библиотеки Entity, базу данных.Сделал функционал со входом и регистрацией

---
+++ Snake/Snake/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Snake
+{
+    public class AppDbContext: DbContext    
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SnakeUser;Integrated Security=True;Connect " +
+                "Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+    }
+}
--- Snake/Snake/MainWindow.xaml.cs
+++ Snake/Snake/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ namespace Snake
         {
             InitializeComponent();
         }
+
         public bool IsDark { get; set; }
         private readonly PaletteHelper paletteHelper = new PaletteHelper();
 
@@ -45,6 +46,47 @@ namespace Snake
         {
             base.OnMouseLeftButtonDown(e);
             DragMove();
+        }
+
+        private void signupBtn_2_Click(object sender, RoutedEventArgs e)
+        {
+            var login = txtUsername_2.Text;
+
+            var passwo = txtPassword_2.Password;
+
+            var isad = 1;
+
+            var sc = 0;
+
+            var context = new AppDbContext();
+
+            var user_exists = context.Users.FirstOrDefault(x => x.Login == login);
+            if (user_exists is not null)
+            {
+                MessageBox.Show("Такой пользователь уже имеется");
+                return;
+            }
+            var user =new User { Login = login, Password = passwo, IsAdmin =isad, Score =sc };
+            context.Users.Add(user);
+            context.SaveChanges();
+            MessageBox.Show("Добро пожаловать пупсик");
+        }
+
+        private void loginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var login = txtUsername.Text;
+
+            var passwo = txtPassword.Password;
+
+            var context = new AppDbContext();
+
+            var user = context.Users.SingleOrDefault(x => x.Login == login && x.Password == passwo);
+            if (user is null)
+            {
+                MessageBox.Show("Неправильный логин или пароль");
+                return;
+            }
+            MessageBox.Show("Ты зашёл в аккаунт пупсик");
         }
     }
 }
